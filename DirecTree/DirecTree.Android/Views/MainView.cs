e0f099cfa64463c3ab8e34748d8564f279679311
Luke Davids <davids_luke@yahoo.com>
2017-01-19 22:06:50 +0200

    Integrated Google Maps

--- DirecTree/DirecTree.Android/Views/MainView.cs
+++ DirecTree/DirecTree.Android/Views/MainView.cs
@@ -1,16 +1,34 @@
 using Android.App;
+using Android.Gms.Maps;
 using Android.OS;
 using MvvmCross.Droid.Views;
 
 namespace DirecTree.Android.Views
 {
     [Activity(Label = "View for MainViewModel")]
-    public class MainView : MvxActivity
+    public class MainView : MvxActivity, IOnMapReadyCallback
     {
+        private GoogleMap _map;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.MainView);
+
+            SetupMap();
+        }
+
+        private void SetupMap()
+        {
+            if (_map == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.googleMapsFragment).GetMapAsync(this);
+
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            _map = googleMap;
         }
     }
 }
