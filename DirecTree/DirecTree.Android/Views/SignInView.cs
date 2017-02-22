ac98ccf1f121e29ffba30a976acfafc8fba993c8
Luke Davids <davids_luke@yahoo.com>
2017-02-22 07:54:26 +0200

    - Replaced icon with a smaller one - Made some visual changes to the login screen - Made some visual changes to the splash screen

--- DirecTree/DirecTree.Android/Views/SignInView.cs
+++ DirecTree/DirecTree.Android/Views/SignInView.cs
@@ -1,15 +1,17 @@
 using Android.App;
 using Android.OS;
 using DirecTree.Android.Views.Base;
+using MvvmCross.Droid.Views;
 
 namespace DirecTree.Android.Views.Fragments
 {
     [Activity(Label = "Sign In", Theme = "@style/MyTheme.MainTheme")]
-    public class SignInView : BaseView
+    public class SignInView : MvxActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            
             SetContentView(Resource.Layout.SignInView);
         }
     }
